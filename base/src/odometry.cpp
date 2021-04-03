1c280f7352e5ca2ddeeecb49d1531891d21247e9
jonathanTIE <jonathan_bleu@hotmail.fr>
2021-04-04 00:32:16 +0200

    move closest wall coordinate to utils

--- base/src/odometry.cpp
+++ base/src/odometry.cpp
@@ -150,34 +150,7 @@ namespace Odometry{
 		#endif
 	}
 
-	float closest_wall_coordinate(int x, int y)
-	{
-		const float LEFT_WALL = 0;
-		const float RIGHT_WALL = 3000; //TO CHECK;
-		const float TOP_WALL = 1000;
-		const float BOTTOM_WALL = 0; //TO CHECK;
-		if(x<=100) //if distance to wall "calculated" from odometry is inferior to 100 mm
-		{
-			return LEFT_WALL;
-		}
-		else if(x>= 2900)
-		{
-			return RIGHT_WALL;
-		}
-		else if(y<=100)
-		{
-			return BOTTOM_WALL;
-		}
-		else if(y>=1900)
-		{
-			return TOP_WALL;
-		}
-		else
-		{
-			return -1.0f;
-		}
-		
-	}
+	
 }
 
 
