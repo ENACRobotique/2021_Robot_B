5f9f9d031e3896a2526d50dfb9f8b69853ce6621
EnacRobotique <robotique.enac@gmail.com>
2021-05-19 17:22:45 +0200

    added library & test code

---
+++ test_carte_secondaire/test_fonctionnement_carte_2/test_fonctionnement_carte_2.ino
@@ -0,0 +1,31 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x65);
+// you can also call it with a different address you want
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+// you can also call it with a different address and I2C interface
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40, Wire);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("GPIO test!");
+
+  pwm.begin();
+  pwm.setPWMFreq(1000);  // Set to whatever you like, we don't use it in this demo!
+
+  // if you want to really speed stuff up, you can go into 'fast 400khz I2C' mode
+  // some i2c devices dont like this so much so if you're sharing the bus, watch
+  // out for this!
+  Wire.setClock(400000);
+}
+
+void loop() {
+  // Drive each pin in a 'wave'
+  for (uint8_t pin=0; pin<16; pin++) {
+    pwm.setPWM(pin, 4096, 0);       // turns pin fully on
+    delay(100);
+    pwm.setPWM(pin, 0, 4096);       // turns pin fully off
+  }
+}
