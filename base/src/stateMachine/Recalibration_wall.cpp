5a0b8b2bf0ea037a7d5886efba9d23881360e16e
jonathanTIE <jonathan_bleu@hotmail.fr>
2021-05-25 23:58:23 +0200

    recalibration enfaite pas besoin de timer, géré ds fsm

--- base/src/stateMachine/Recalibration_wall.cpp
+++ base/src/stateMachine/Recalibration_wall.cpp
@@ -32,32 +32,25 @@ void Recalibration_wall::leave() {
 }
 
 void Recalibration_wall::doIt() {
-	if(true) //TODO : timer délai d'intervalle minimum
+
+	float cur_reading;
+	if(isX)
 	{
-
-		float cur_reading;
-		if(isX)
-		{
-			cur_reading = 	Odometry::get_motor_x();
-		}
-		else
-		{
-			cur_reading = Odometry::get_motor_y();
-		}
-		if(fabs(cur_reading-motor_cod_last_reading) <= 0.001f && motor_cod_last_reading != -1)
-		{
-			navigator.forceStop();
-			float x = (isX) ? targetPos : Odometry::get_motor_x();
-			float y = (!isX) ? targetPos : Odometry::get_motor_y();
-			Odometry::set_pos(x,y, targetTheta);
-			fsmSupervisor.setNextState(NULL);
-		}
-		motor_cod_last_reading = cur_reading;
+		cur_reading = 	Odometry::get_motor_x();
 	}
-
-
-	
-
+	else
+	{
+		cur_reading = Odometry::get_motor_y();
+	}
+	if(fabs(cur_reading-motor_cod_last_reading) <= 0.001f && motor_cod_last_reading != -1)
+	{
+		navigator.forceStop();
+		float x = (isX) ? targetPos : Odometry::get_motor_x();
+		float y = (!isX) ? targetPos : Odometry::get_motor_y();
+		Odometry::set_pos(x,y, targetTheta);
+		fsmSupervisor.setNextState(NULL);
+	}
+	motor_cod_last_reading = cur_reading;
 
 }
 
