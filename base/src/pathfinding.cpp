4fada1d1dc5f94cdc605292781d80ae2257e5488
KirrimK <KirrimK@users.noreply.github.com>
2021-06-09 00:23:09 +0200

    wip pathfinding & del lid libs since xv11 is ded (repose en paix petit ange parti trop tôt)

--- base/src/pathfinding.cpp
+++ base/src/pathfinding.cpp
@@ -1,2 +1,58 @@
 #include "pathfinding.h"
-#include "lidar/LidarXV11.h"
+#include "params.h"
+#include <math.h>
+
+Geom_Vec::Geom_Vec(float x, float y){
+    this->x = x;
+    this->y = y;
+}
+
+Geom_Vec::Geom_Vec(float start[2], float end[2]){
+    this->x = end[0] - start[0];
+    this->y = end[1] - start[1];
+}
+
+Geom_Vec Geom_Vec::scale(float factor){
+    return Geom_Vec(x*factor, y*factor);
+}
+
+float Geom_Vec::length(){
+    return sqrtf(x*x + y*y);
+}
+
+float Geom_Vec::dot(Geom_Vec other){
+    return x*other.x + y*other.y;
+}
+
+float Geom_Vec::get_angle(){
+    if (x != 0){
+        float at = (float) atan(y/x)*360/(2*PI);
+        if (x >= 0){ return at; }
+        else{ return 180 + at; }
+    }
+    else{
+        if (y>0){ return 90; }
+        else if (y < 0){ return -90; }
+        else{ return 0; }
+    }
+}
+
+float Geom_Vec::dist_to_point(float point[2]){
+    Geom_Vec vec_point(point[0], point[1]);
+    float len_line = this->length();
+    Geom_Vec sc_line = this->scale(1/len_line);
+    Geom_Vec sc_ptvec = vec_point.scale(1/len_line);
+    float t = sc_line.dot(sc_ptvec);
+    if (t < 0){
+        t = 0;
+    }
+    else if (t > 1) {
+        t = 1;
+    }
+    Geom_Vec nearest = this->scale(t);
+    float nearest_coords[2] = {nearest.x, nearest.y};
+    float dist = Geom_Vec(point, nearest_coords).length();
+    return dist;
+}
+
+
--- base/src/pathfinding.h
+++ base/src/pathfinding.h
@@ -1,5 +1,93 @@
 #ifndef PATHFINDING
 #define PATHDINFING
 
+#include "lidar/Lidar.h"
+
+const int MAX_WP = 30; /* à augmenter si bcp de points ajoutés */
+const unsigned int NO = 65535; /* entier max arduino non-signé, "approche l'infini" */
+const unsigned int YES = 4000; /* raccourci pour connecter deux wp sur le graphe sans calculer la vraie distance */
+const unsigned int SHORT = 3500; /* raccourci pour connecter deux wp sur le graphe sans calculer la vraie distance, avec un chemin avantagé */
+const unsigned int LONGR = 4500; /* raccourci pour connecter deux wp sur le graphe sans calculer la vraie distance, avec un chemin désavantagé */
+
+struct Waypoint
+{
+    /* coordonées sur le terrain, en mm, depuis le coin bleu sud */
+    int x;
+    int y;
+    /* liste de distances séparant le waypoint des autres sur la carte */
+    int wp_adj[MAX_WP];
+};
+
+/* Un graphe de distance entre des Waypoints*/
+struct Graph
+{
+    /* le nombre de wp présents dans le graphe */
+    int wp_number;
+    /* La liste des Waypoints concernés (attention à l'ordre) */
+    Waypoint* wp_list[MAX_WP];
+    /* le graphe */
+    int graph[MAX_WP][MAX_WP];
+};
+
+/* Désigne une suite de points à emprunter pour se rendre d'un point A à un point B via les Waypoints*/
+struct Route
+{
+    /* Quantité de points à emprunter (destination comptée, point de départ omis) */
+    int length;
+    /* Référence au Waypoint de départ*/
+    Waypoint &depart;
+    /* Référence au Waypoint de destination */
+    Waypoint &destination;
+    /* Liste des Waypoints à emprunter dans l'ordre */
+    Waypoint* wp_list[MAX_WP];
+    /* La route ne mène pas à la destination (dijkstra ne donne pas de solution) */
+    bool stuck;
+    /* La route est libre immédiatement */
+    bool isfree;
+    /* La route est la plus courte, sans compter les éventuels obstacles */
+    bool isshortest;
+};
+
+/* Élement retourné par dijkstra */
+struct DijkstraResult{
+    /* Index du point de départ dans le graphe */
+    int dep_index;
+    /* Liste des distances depuis le point de départ */
+    int dist[MAX_WP];
+    /* Liste des parents de chaque waypoint */
+    int parent[MAX_WP];
+};
+
+/* Un chemin qui pourrait être une route mais n'a pas encore été confirmée ou vérifiée*/
+struct PseudoRoute{
+    /* Les indices des sommets du graphe à parcourir */
+    int parcours[MAX_WP];
+    /* Le chemin amène t'il à la destination */
+    bool reaches;
+};
+
+class Geom_Vec{
+    public:
+        Geom_Vec(float x, float y);
+        Geom_Vec(float start[2], float end[2]);
+        Geom_Vec scale(float factor);
+        float length();
+        float dot(Geom_Vec other); 
+        float get_angle();
+        float dist_to_point(float point[2]);
+    private:
+        float x;
+        float y;
+};
+
+namespace ATC{
+    Graph generate_graph(Waypoint *waypoint_list[MAX_WP]);
+    Route find_route(Graph &graph, int depart[2], int destination[2], Lidar lidar, float robot_pos[3]);
+    bool is_path_blocked(int start[2], int end[2], Lidar lidar, float robot_pos[3]);
+    int min_dist(int *dist[MAX_WP], bool *Dset[MAX_WP]);
+    DijkstraResult dijkstra_crap(Graph graph, int src_index);
+    Geom_Vec from_pol_to_abs(float robot_pos[3], int lid_ang, int lid_dist);
+
+}
 
 #endif //PATHFINDING
