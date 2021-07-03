4214e5b818081feb187d92bf68cbb0562f6176ef
EnacRobotique <robotique.enac@gmail.com>
2021-07-03 18:21:30 +0200

    updated lidar data

--- base/src/lidar/LidarData.h
+++ base/src/lidar/LidarData.h
@@ -8,11 +8,13 @@ class LidarData{
         LidarData();
         float get_distance(int angle);
         byte get_quality(int angle);
+        float get_time(int angle);
         void set_data(int angle, float distance, byte quality);
     
     private:
         float distance[360];
         byte quality[360];
+        float last_time[360];
 };
 
 #endif //LIDAR_DATA
