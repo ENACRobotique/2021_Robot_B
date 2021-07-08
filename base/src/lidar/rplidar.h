000b47c1be63298cc9092b4d079c0cdaf709243b
jonathanTIE <jonathan_bleu@hotmail.fr>
2021-07-08 10:11:08 +0200

    added

--- base/src/lidar/rplidar.h
+++ base/src/lidar/rplidar.h
@@ -1,45 +1,85 @@
-/*
- *  RPLIDAR SDK
- *
- *  Copyright (c) 2009 - 2014 RoboPeak Team
- *  http://www.robopeak.com
- *  Copyright (c) 2014 - 2018 Shanghai Slamtec Co., Ltd.
- *  http://www.slamtec.com
- *
- */
-/*
- * Redistribution and use in source and binary forms, with or without 
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, 
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice, 
- *    this list of conditions and the following disclaimer in the documentation 
- *    and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
- * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
- * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
-#pragma once
-
+#include "rplidar.h"
 #include "Arduino.h"
-#include <vector>
-#include "types.h"
-#include "rplidar_protocol.h"
-#include "rplidar_cmd.h"
 
-#include "rplidar_driver.h"
+unsigned long LIDAR_LASTMSG = millis();
 
-#define RPLIDAR_SDK_VERSION  "1.9.0"
+void checkAndRestartLidar(){
+    if (millis() - LIDAR_LASTMSG > 500UL){
+        SerialCtrl.println("restarting lidar");
+        LIDAR_LASTMSG = millis();
+        Serial1.write(0xA5); 
+        Serial1.write(0x20);
+    }
+}
+
+void readLidar() {
+ uint8_t current;
+ static uint8_t n = 0;
+ static uint8_t quality;
+ static bool start;
+ static uint16_t angleq6;
+ static uint16_t distanceq2;
+ 
+ float angle;
+ float distance;
+
+ while(Serial1.available()) {
+    // SerialCtrl.print("size ");
+    // SerialCtrl.println(Serial1.available());
+  current = Serial1.read();
+
+  switch(n) {
+
+   case 0:
+    if((current & 1) != (current >> 1 & 1)) {
+     quality = current >> 2;
+     start = current & 1;
+     n = 1;
+    }
+   break;
+
+   case 1:
+    if(current & 1) {
+     angleq6 = current >> 1;
+     n = 2;
+    } else
+     n = 0;
+   break;
+
+   case 2:
+    angleq6 |= current << 7;
+    n = 3;
+   break;
+
+   case 3:
+    distanceq2 = current;
+    n = 4;
+   break;
+
+   case 4:
+    distanceq2 |= current << 8;
+    n = 0;
+    if(distanceq2) { // Si la lecture est valide
+     angle = float(angleq6) / 64.0;
+     distance = float(distanceq2) / 4.0;
+     int nearest = (180+int(angle+0.5f))%360;
+     LIDAR_LASTMSG = millis();
+
+    if ((int)millis()%50 == 0){
+        SerialCtrl.print(nearest);
+    SerialCtrl.print("\t");
+    SerialCtrl.print(ATC::lidar.get_distance(nearest));
+    SerialCtrl.print("\t");
+    SerialCtrl.print(ATC::lidar.get_quality(nearest));
+    SerialCtrl.print("\n");
+    }
+    
+    
+    ATC::lidar.set_data(nearest, distance, quality);
+
+
+    }
+   break;
+  }
+ }
+}
